a5c6e9552c50ebe3c7428caad88f46ff2bafb28a
Gabriel Pimentel <gabrielcosta.tads@gmail.com>
2019-11-06 15:44:46 -0300

    Definicao das Variaveis

--- distancia.c
+++ distancia.c
@@ -1,25 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 
+//Função que calcula a distancia entre dois pontos
 float calculaDistancia(int x1, int y1, int x2, int y2){
 	return sqrt(pow(x2-x1, 2)+pow(y2-y1, 2));
 }
 int main(){
-	int x1, y1, x2, y2;
-	
-	printf('Digite o valor para ');
-	scanf("%d", &x1);
-	
-	printf('Digite o valor para ');
-	scanf("%d", &y1);
-	
-	printf('Digite o valor para ');
-	scanf("%d", &x2);
-	
-	printf('Digite o valor para ');
-	scanf("%d", &y2);
-	
-	calculaDistancia(x1, y1, x2, y2);
-	
-	return 0;
+	//Definição das variaveis
+	int x1,y1,x2,y2;
+	float distancia;
 }
